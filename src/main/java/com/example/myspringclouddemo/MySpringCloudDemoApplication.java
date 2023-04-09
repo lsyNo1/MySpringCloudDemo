7429f6a2e19879460fb82ab4f0e107ef11e59cb1
lsy <91182641+lsyNo1@users.noreply.github.com>
2023-04-09 16:07:52 +0800

    add file

---
+++ src/main/java/com/example/myspringclouddemo/MySpringCloudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.myspringclouddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringCloudDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringCloudDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myspringclouddemo/MySpringCloudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myspringclouddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringCloudDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
