e10b7c398ce3211bee493cc78fc93240610c05d8
lsy <91182641+lsyNo1@users.noreply.github.com>
2023-04-10 10:05:19 +0800

    add file

---
+++ springCloud-persistence/src/main/java/User.java
@@ -0,0 +1,53 @@
+import java.io.Serializable;
+
+/**
+ * Created by lance on 2023/4/9.
+ */
+public class User implements Serializable {
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+    private Long areaId;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getAreaId() {
+        return areaId;
+    }
+
+    public void setAreaId(Long areaId) {
+        this.areaId = areaId;
+    }
+}
